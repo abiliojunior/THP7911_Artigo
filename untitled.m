4c0fe42d1b53ca2f0265e75588c7493ca28d8f90
abiliojunior <abiliojunior.mat@gmail.com>
2025-08-05 22:14:18 -0300

    Inicial

---
+++ untitled.m
@@ -0,0 +1,11 @@
+%--------------------------------------------------------------------------
+% DISCIPLINA: THP7911 - CONTROLE ADAPTATIVO PREDITIVO
+% PROGRAMA DE PÓS-GRADUAÇÃO EM ENGENHARIA ELÉTRICA.
+% MATRICULA: 559990
+% ALUNO: Abílio Costa da Silva Júnior
+% -------------------------------------------------------------------------
+% Trabalho 01 em MATLAB
+% calcular as prediçoes do modelo de pertubações:
+% e(k) = (1 + a1q^-1 + a2q^-2)n(k)
+% Usando Eq. Diofantina
+%--------------------------------------------------------------------------
